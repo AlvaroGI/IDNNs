377668bc634a04d701e273cf3725d6fe1576fe9e
AlvaroGI <31722017+AlvaroGI@users.noreply.github.com>
2018-06-16 20:42:51 -0400

    Add files via upload

---
+++ tune_lambdaR.sh
@@ -0,0 +1,39 @@
+python main.py -lambdaR 0.0
+
+python main.py -lambdaR 0.0
+
+python main.py -lambdaR 0.0
+
+python main.py -lambdaR 0.0
+
+python main.py -lambdaR 0.0
+
+python main.py -lambdaR 0.001
+
+python main.py -lambdaR 0.001
+
+python main.py -lambdaR 0.001
+
+python main.py -lambdaR 0.001
+
+python main.py -lambdaR 0.001
+
+python main.py -lambdaR 0.0001
+
+python main.py -lambdaR 0.0001
+
+python main.py -lambdaR 0.0001
+
+python main.py -lambdaR 0.0001
+
+python main.py -lambdaR 0.0001
+
+python main.py -lambdaR 0.00001
+
+python main.py -lambdaR 0.00001
+
+python main.py -lambdaR 0.00001
+
+python main.py -lambdaR 0.00001
+
+python main.py -lambdaR 0.00001
