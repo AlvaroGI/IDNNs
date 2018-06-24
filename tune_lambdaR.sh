323de2cf415f4bd6cf4e952d4a1bd7d1702dbf14
AlvaroGI <alvarogomezinesta@gmail.com>
2018-06-24 00:53:36 -0400

    Running in Dell

--- tune_lambdaR.sh
+++ tune_lambdaR.sh
@@ -37,3 +37,123 @@ python main.py -lambdaR 0.00001
 python main.py -lambdaR 0.00001
 
 python main.py -lambdaR 0.00001
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.01
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 0.1
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 1.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaR 10.0
+
+python main.py -lambdaF 10.0
+
+python main.py -lambdaF 10.0
+
+python main.py -lambdaF 10.0
+
+python main.py -lambdaF 10.0
+
+python main.py -lambdaF 10.0
+
+python main.py -lambdaF 1.0
+
+python main.py -lambdaF 1.0
+
+python main.py -lambdaF 1.0
+
+python main.py -lambdaF 1.0
+
+python main.py -lambdaF 1.0
+
+python main.py -lambdaF 0.1
+
+python main.py -lambdaF 0.1
+
+python main.py -lambdaF 0.1
+
+python main.py -lambdaF 0.1
+
+python main.py -lambdaF 0.1
+
+python main.py -lambdaF 0.01
+
+python main.py -lambdaF 0.01
+
+python main.py -lambdaF 0.01
+
+python main.py -lambdaF 0.01
+
+python main.py -lambdaF 0.01
